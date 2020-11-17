1684a0617841e8c048c9644acb61e8396d6cc8f2
yuanlijian <yuanlijianvip@163.com>
2020-11-17 14:37:39 +0800

    请求配置

--- src/common/component/header.js
+++ src/common/component/header.js
@@ -1,6 +1,16 @@
 import React from "react";
+import http from "../../store/action/http";
 
 export default function Header() {
+  http.post("/lecturer/lists?page=1&rows=20",
+  {
+    order: "desc",
+    sort: "id",
+    category_id: 1,
+    recommend: 1
+  }).then((res)=>{
+    console.log(res);
+  })
   return (
     <header id="header">
       <nav className="menu">
